5dd6ed19fe42fdc45d5f3d136a2ede398bdafae1
PAVAN30627 <vvce21cse0055@vvce.ac.in>
2025-03-02 21:38:10 +0530

    Arrays

---
+++ Arrays/MinArrayElement.java
@@ -0,0 +1,39 @@
+// Q. Find the smallest number in an array
+package TCS_NQT.Arrays;
+
+import java.util.*;
+public class MinArrayElement {
+    public static void main(String[] args)
+    {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int size = input.nextInt();
+
+        if(size <= 0)
+        {
+            System.out.println("Invalid array size!");
+            return;
+        }
+
+        System.out.println("Enter array elements:");
+        int[] arr = new int[size];
+        for(int i = 0;i < size;i++)
+        {
+            arr[i] = input.nextInt();
+        }
+
+        int result = findSmallest(arr);
+        System.out.println("Minimum Array Element is: " + result);
+        input.close();
+    }
+
+    private static int findSmallest(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for(int num : arr)
+        {
+            if(num < min) min = num;
+        }
+        return min;
+    }
+}
